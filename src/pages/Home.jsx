21546d44d5debabae16e0e52c1181844796dfe6d
박준혁 <201921001@sangmyung.kr>
2024-07-24 18:14:57 +0900

    main페이지제작중과 header부분 라우터변경

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
-const Home = () => {
-  const navigate = useNavigate();
-  return <div>메인 페이지</div>;
-};
-
-export default Home;
